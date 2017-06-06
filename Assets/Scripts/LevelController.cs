45b36096a6cdd009357447aa94f0b650f63b5c54
alec100_94 <alec400@gmail.com>
2017-06-06 16:20:50 +0100

    Refactored, Parts of Player Controller into Level Controller, cleaner code.

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -18,17 +18,57 @@ public class LevelController : MonoBehaviour {
     private GameObject goal;
     private AsyncOperation loadNextLevel;
 
-	// Use this for initialization
-	void Start () {
+    private Vector3 initialPlayerPosition;
+    private Vector3 initialCamPosition;
+    private GameObject levelComplete;
+    private Rigidbody playerBody;
+
+    public bool goalReached;
+    public float timeSinceGoalReached;
+
+    // Use this for initialization
+    void Start () {
         moving = true;
         canvas = GameObject.FindObjectOfType<Canvas>().GetComponent<Canvas>();
         coinsCollectedText = canvas.transform.FindChild("CoinsCollected").GetComponent<Text>();
         goal = transform.FindChild("Goal").gameObject;
         loadNextLevel = LoadNextLevel();
-	}
+
+        levelComplete = GameObject.Find("Level Complete");
+        playerBody = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>();
+        initialPlayerPosition = playerBody.transform.position;
+        initialCamPosition = Camera.main.transform.position;
+        Respawn();
+    }
 
     void Update () {
         coinsCollectedText.text = "Coins Collected: " + coinsCollected;
+
+        if (goalReached)
+        {
+            if (timeSinceGoalReached > 0.3f)
+            {
+                levelComplete.SetActive(true);
+                levelComplete.transform.FindChild("Coins").GetComponent<Text>().text = coinsCollected + "/" + new List<CoinController>(transform.FindChild("Coins").GetComponentsInChildren<CoinController>()).FindAll(i => i.gameObject.activeSelf).Count + " Coins Collected";
+                levelComplete.transform.FindChild("ReRun").GetComponent<Button>().onClick.AddListener(() => Respawn());
+                levelComplete.transform.FindChild("NextLevel").GetComponent<Button>().onClick.AddListener(() => NextLevel());
+            }
+        }
+
+        if ((Camera.main.WorldToScreenPoint(playerBody.transform.position).x >= Camera.main.pixelWidth * 0.66f) && moving)
+        {
+            multiplier = 1.5f;
+            Camera.main.transform.position += Vector3.right * 0.07f;
+        }
+        else if ((Camera.main.WorldToScreenPoint(playerBody.transform.position).x >= Camera.main.pixelWidth * 0.5f) && moving)
+        {
+            multiplier = 1.0f;
+            Camera.main.transform.position += Vector3.right * 0.05f;
+        }
+        else
+        {
+            playerBody.AddForce(Vector3.right * 0.008f);
+        }
     }
 
 	// Update is called once per frame
@@ -61,5 +101,26 @@ public class LevelController : MonoBehaviour {
     {
         if(loadNextLevel != null)
             loadNextLevel.allowSceneActivation = true;
+    }
+
+    public void Respawn()
+    {
+        levelComplete.SetActive(false);
+        transform.position = new Vector3(0.6f, -0.5f, -0.4f);
+        moving = true;
+        goalReached = false;
+
+        var coins = transform.FindChild("Coins").GetComponentsInChildren<CoinController>();
+        foreach (var coin in coins)
+            coin.GetComponent<Renderer>().enabled = true;
+
+        timeSinceGoalReached = 0;
+        coinsCollected = 0;
+        multiplier = 0;
+        playerBody.velocity = Vector3.zero;
+        playerBody.angularVelocity = Vector3.zero;
+        Camera.main.transform.position = initialCamPosition;
+        playerBody.transform.position = initialPlayerPosition;
+
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,42 +11,16 @@ public class PlayerController : MonoBehaviour
     private new Rigidbody rigidbody;
     private LevelController level;
 
-    /* Move to Level Class */
-    private bool goalReached;
-    private float timeSinceGoalReached;
-    private Vector3 initialPosition;
-    private Vector3 initialCamPosition;
-    private GameObject levelComplete;
-
     private void Start()
     {
         animator = GetComponent<Animator>();
         rigidbody = GetComponent<Rigidbody>();
         level = GameObject.Find("Level").GetComponent<LevelController>();
-
-        /* Move to Level Class */
-        levelComplete = GameObject.Find("Level Complete");
-        initialPosition = transform.position;
-        initialCamPosition = Camera.main.transform.position;
-        Respawn();
     }
-
 
     private void Update()
     {
-        /* Move to Level Class */
-        if (goalReached)
-        {
-            if (timeSinceGoalReached > 0.3f)
-            {
-                levelComplete.SetActive(true);
-                levelComplete.transform.FindChild("Coins").GetComponent<Text>().text = level.coinsCollected + "/" + new List<CoinController>(level.transform.FindChild("Coins").GetComponentsInChildren<CoinController>()).FindAll(i => i.gameObject.activeSelf).Count + " Coins Collected";
-                levelComplete.transform.FindChild("ReRun").GetComponent<Button>().onClick.AddListener(() => Respawn());
-                levelComplete.transform.FindChild("NextLevel").GetComponent<Button>().onClick.AddListener(() => level.NextLevel());
-            }
-
-            return;
-        }
+        if (level.goalReached) return;
 
         if ((CrossPlatformInputManager.GetButtonDown("Jump") || Input.GetMouseButtonDown(0)) && !jumping)
         {
@@ -65,22 +39,6 @@ public class PlayerController : MonoBehaviour
             level.multiplier = 0.7f;
         }
 
-        /* Move to Level Class */
-        if ((Camera.main.WorldToScreenPoint(transform.position).x >= Camera.main.pixelWidth * 0.66f) && level.moving)
-        {
-            level.multiplier = 1.5f;
-            Camera.main.transform.position += Vector3.right * 0.07f;
-        }
-        else if ((Camera.main.WorldToScreenPoint(transform.position).x >= Camera.main.pixelWidth * 0.5f) && level.moving)
-        {
-            level.multiplier = 1.0f;
-            Camera.main.transform.position += Vector3.right * 0.05f;
-        }
-        else
-        {
-            rigidbody.AddForce(Vector3.right * 0.008f);
-        }
-
         if (rigidbody.velocity.x < 0)
         {
             rigidbody.velocity = new Vector3(0, rigidbody.velocity.y, rigidbody.velocity.z);
@@ -90,37 +48,12 @@ public class PlayerController : MonoBehaviour
         {
             rigidbody.velocity = new Vector3(6, rigidbody.velocity.y, rigidbody.velocity.z);
         }
-
-
-    }
-
-    /* Move to Level Class */
-    private void Respawn()
-    {
-        levelComplete.SetActive(false);
-        level.transform.position = new Vector3(0.6f, -0.5f, -0.4f);
-        level.moving = true;
-        goalReached = false;
-
-        var coins = level.transform.FindChild("Coins").GetComponentsInChildren<CoinController>();
-        foreach (var coin in coins)
-            coin.GetComponent<Renderer>().enabled = true;
-
-        
-        timeSinceGoalReached = 0;
-        level.coinsCollected = 0;
-        level.multiplier = 0;
-        rigidbody.velocity = Vector3.zero;
-        rigidbody.angularVelocity = Vector3.zero;
-        Camera.main.transform.position = initialCamPosition;
-        transform.position = initialPosition;
-
     }
 
     private void OnCollisionStay(Collision collision)
     {
         if(collision.gameObject.tag == "Goal")
-            timeSinceGoalReached += Time.deltaTime;
+            level.timeSinceGoalReached += Time.deltaTime;
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -131,12 +64,12 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.tag == "Goal")
         {
             animator.SetFloat("Forward", 0);
-            goalReached = true;
+            level.goalReached = true;
         }
 
        if(collision.gameObject.tag == "Respawn")
        {
-            Respawn();
+            level.Respawn();
        }
     }
 }
